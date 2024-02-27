091d76c05f961f34fdd9b77804c0c902aef4009c
Abdullah2599 <abdullahcloudupload@gmail.com>
2024-02-27 09:48:14 +0500

    Update index.dart

--- app/index.dart
+++ app/index.dart
@@ -1,33 +1,44 @@
 void main() {
-    int age =26;
-    double percentage =88.88;
-    bool tvalue =false;
-    bool fvalue =true;
-    String name = "Sharjeel";
-    List arrr=[1,2,3];
-    Map data= {
-        "name": "Abc",
-        "age": 25,
-        "salary": 50000
+//     int age =26;
+//     double percentage =88.88;
+//     bool tvalue =false;
+//     bool fvalue =true;
+//     String name = "Sharjeel";
+//     List arrr=[1,2,3];
+//     Map data= {
+//         "name": "Abc",
+//         "age": 25,
+//         "salary": 50000
 
-    };
-   int num1, num2, result;
-    num1 =12;
-    num2 =17;
+//     };
+//    int num1, num2, result;
+//     num1 =12;
+//     num2 =17;
 
     
-    result= num1+ num2;
-    print('Result of two numbers: $result');
+//     result= num1+ num2;
+//     print('Result of two numbers: $result');
 
-    print("Welcome");
-    print('Age: $age');
-    print(name);
-    print(arrr[2]);
-    print(data["name"]);
-    print(fvalue);
-    print(tvalue);
-    print(percentage);
+//     print("Welcome");
+//     print('Age: $age');
+//     print(name);
+//     print(arrr[2]);
+//     print(data["name"]);
+//     print(fvalue);
+//     print(tvalue);
+//     print(percentage);
 
+
+double feet = 2000 , meter , km;
+meter = feet / 3;
+print('Meters: $meter');
+ 
+km = meter / 1000;
+print ('Kilometer: $km');
+
+String Country = "Pakistan";
+print('Length:${Country.length}');
+print(Country[2]);
 
 
   
