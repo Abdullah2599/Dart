526879dbd6cd641897b4598b173d40da2e18634b
Abdullah2599 <abdullahcloudupload@gmail.com>
2024-02-27 09:34:57 +0500

    Create index.dart

---
+++ app/index.dart
@@ -0,0 +1,34 @@
+void main() {
+    int age =26;
+    double percentage =88.88;
+    bool tvalue =false;
+    bool fvalue =true;
+    String name = "Sharjeel";
+    List arrr=[1,2,3];
+    Map data= {
+        "name": "Abc",
+        "age": 25,
+        "salary": 50000
+
+    };
+   int num1, num2, result;
+    num1 =12;
+    num2 =17;
+
+    
+    result= num1+ num2;
+    print('Result of two numbers: $result');
+
+    print("Welcome");
+    print('Age: $age');
+    print(name);
+    print(arrr[2]);
+    print(data["name"]);
+    print(fvalue);
+    print(tvalue);
+    print(percentage);
+
+
+
+  
+}
