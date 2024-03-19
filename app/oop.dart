d0f78eca33a1cf84195a1bceac47428ded29978e
Abdullah2599 <abdullahcloudupload@gmail.com>
2024-03-19 10:20:54 +0500

    check

---
+++ app/oop.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+
+main(){
+
+
+
+
+// user use = new user();
+// print("your roll no. is: ${use.rollno= 66}");
+// use.stdname="abc";
+// use.functions();
+
+
+person p =new person();
+stdout.write("Enter your ID: ");
+int id =int.parse(stdin.readLineSync()!);
+print("Enter your Name");
+String? name = stdin.readLineSync();
+stdout.write("Enter your Age: ");
+int? age =int.parse(stdin.readLineSync()!);
+stdout.write("Enter your Address: ");
+var address =stdin.readLineSync();
+
+
+p.display(id,name,age,address);
+}
+
+
+
+
+
+
+
+
+
+// class user{
+//   int rollno=0;
+//   var stdname;
+
+//   functions(){
+//     print("Your Good name is: "+ stdname);
+//   }
+// }
+
+
+class person{
+
+  int? id;
+  String? name;
+  int? age;
+  String? address;
+
+  void display(int? id, String? name,int? age,String? address){
+    print("Your id is: $id");
+    print("Your name is: $name");
+    print("Your age is: $age");
+    print("Your address is: $address");
+  }
+}
