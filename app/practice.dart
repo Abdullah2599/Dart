25913d2c0507cf3bd7e2c368715125eaf51d7fb7
Abdullah2599 <abdullahcloudupload@gmail.com>
2024-03-20 21:50:54 +0500

    Create practice.dart

---
+++ app/practice.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  String fName= 'Andrea';
+  String lName= 'Bizzetto';
+  int age = 22;
+  double height= 1.84;
+
+  print(fName);
+  print(lName);
+  print(age);
+  print(height);
+}
