aed96676af124a3a90ed9081111517798a1ca779
mbilal2024 <abdullahcloudupload@gmail.com>
2024-03-28 09:56:24 +0500

    check

--- app/practice.dart
+++ app/practice.dart
@@ -1,4 +1,5 @@
-import 'dart:io';
+// import 'dart:ffi';
+// import 'dart:io';
 
 main(){
 
@@ -10,15 +11,58 @@ main(){
 // simpleinterest.time=2;
 
 // print("Simple interest is ${simpleinterest.Interest()}.");
- print("Enter your Name");
-String? names = stdin.readLineSync()!;
- print("Enter your age");
-int? ag = int.parse(stdin.readLineSync()!);
-users u =users(names,ag);
-u.display();
+//  print("Enter your Name");
+// String? names = stdin.readLineSync()!;
+//  print("Enter your age");
+// int? ag = int.parse(stdin.readLineSync()!);
+// users u =users(names,ag);
+// u.display();
+
+
+
+// info e =info("John",22,"History",50000);
+// e.display();
+
+
+// info f =info();
+// f.display();
+
+
+
 
 
 }
+
+// Fix
+
+// class Chair{
+//   String? name;
+//   String? colour;
+  
+//   Chair c= Chair(${this.name}: "Chair", colour:"Red");
+
+//   void display(){
+//     print("Product Name : ${this.name}");
+//     print("Product Colour : ${this.colour}");
+
+//   }
+// }
+
+
+// class info{
+//   String? name;
+//   String? colour;
+  
+//   info({this.name="Chair",this.colour="Red"});
+
+//   void display(){
+//     print("Product Name : ${this.name}");
+//     print("Product Colour : ${this.colour}");
+
+//   }
+// }
+
+
 
 // class name{
 //   name(){
@@ -26,20 +70,35 @@ u.display();
 //   }
 // }
 
-class users{
-  String? name;
-  int? age;
-  users(String a,int b){
-    this.name=a;
-    this.age=b;
+// class users{
+//   String? name;
+//   int? age;
+//   users(String a,int b){
+//     this.name=a;
+//     this.age=b;
 
-  }
-  void display(){
-    print("Your name is ${name}");
-    print("Your age is ${age}");
-  }
-}
+//   }
+//   void display(){
+//     print("Your name is ${name}");
+//     print("Your age is ${age}");
+//   }
+// }
 
+// class info{
+//   String? name;
+//   int? age;
+//   String? subject;
+//   double? Salary;
+
+//   info(this.name,this.age,this.subject,this.Salary);
+
+//    void display(){
+//     print("Your name is ${this.name}");
+//     print("Your age is ${this.age}");
+//     print("Your Subject is ${this.subject}");
+//     print("Your Salary is ${this.Salary}");
+//   }
+// }
 
 
 // class SimpleInterest{
