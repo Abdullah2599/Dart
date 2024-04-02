f8ed8d1ca6edbcea84fe240d11b87ca8089dfeb1
mbilal2024 <abdullahcloudupload@gmail.com>
2024-04-02 10:25:55 +0500

    ab

---
+++ app/col.dart
@@ -0,0 +1,12 @@
+void main(){
+
+  List<int> numbers= [1,2,3,4,5];
+  Set<String> names={'Alice','Bob','Charlie'};
+  Map<String,int> scores={'Alice':95,'Bob':87,'Charlie':92};
+
+  numbers.add(6);
+  print(numbers);
+  print(names);
+  print(scores);
+  
+}
